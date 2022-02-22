49a8b3c43d70b332b7adcafa2a5ab295237fb100
stansful <stansful@gmail.com>
2022-02-22 11:29:06 +0300

    feat: add galleryService
    updateQueryParams - adds query to the url params
    showGallery - check for expired token and return to the auth page if true, else updateQueryParams, send request to the api endpoint and append received pictures to the gallery
    two events for next and previous buttons they increase/decrease page number and set new page number and showGallery in the end
    
    MODULE-1-PART-3-gallery

---
+++ js/galleryService.js
@@ -0,0 +1,54 @@
+const gallery = document.querySelector('#gallery')
+const previousButton = document.querySelector('#previous')
+const nextButton = document.querySelector('#next')
+
+const queryRegex = /\?page=[1-5]/
+
+if (location.search.match(queryRegex)) {
+    const pageNumber = location.search.split('?page=').pop()
+    setNewPage(pageNumber)
+}
+
+const updateQueryParams = (pageNumber) => {
+    if (location.search !== `?page=${pageNumber}`) {
+        location.search = `?page=${pageNumber}`
+    }
+}
+
+const nextButtonEvent = async () => {
+    const currentPage = getCurrentPage()
+    const newPage = Number(currentPage) + 1
+    setNewPage(newPage)
+    await showGallery(getCurrentPage(), getToken())
+}
+
+const previousButtonEvent = async () => {
+    const currentPage = getCurrentPage()
+    const newPage = Number(currentPage) - 1
+    setNewPage(newPage)
+    await showGallery(getCurrentPage(), getToken())
+}
+
+const showGallery = async (page, token) => {
+    const expired = checkTokenForExpiration()
+
+    if (expired) {
+        removeToken()
+        nextButton.removeEventListener(EVENT_TYPES.click, nextButtonEvent)
+        previousButton.removeEventListener(EVENT_TYPES.click, previousButtonEvent)
+        return document.location.replace('./index.html')
+    }
+
+    updateQueryParams(page)
+
+    const data = await httpGet(`${API_URL}/gallery?page=${page}`, token)
+
+    gallery.innerHTML = ''
+
+    data.objects.forEach(imgLink => gallery.innerHTML += `<img src="${imgLink}" width="200" height="200" alt="img">`)
+}
+
+showGallery(getCurrentPage(), getToken())
+
+nextButton.addEventListener(EVENT_TYPES.click, nextButtonEvent)
+previousButton.addEventListener(EVENT_TYPES.click, previousButtonEvent)
