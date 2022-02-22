2549ccd05795a2f49be4185c0c016e92cea7f5fc
stansful <stansful@gmail.com>
2022-02-22 10:11:54 +0300

    feat: add constants
    add constants file to avoid multiplicity of the same variables
    
    MODULE-1-PART-3-gallery-auth

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+const API_URL = 'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api'
+const VALIDATION_FAILED = 'Please re-enter password & username'
+const EVENT_TYPES = {
+    submit: 'submit'
+}
+const TOKEN = 'token'
+const TOKEN_EXPIRES_TIME = 'tokenExpiresTime'
+const TOKEN_TIME_TO_LIVE_IN_MINUTES = 10
