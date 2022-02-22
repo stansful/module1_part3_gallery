752e4de556ab2c40cbebc6b34106c557f03cfbb9
stansful <stansful@gmail.com>
2022-02-22 11:13:47 +0300

    feat: add page and click event type into constants
    MODULE-1-PART-3-gallery

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,10 @@
 const API_URL = 'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api'
 const VALIDATION_FAILED = 'Please re-enter password & username'
 const EVENT_TYPES = {
-    submit: 'submit'
+    submit: 'submit',
+    click: 'click'
 }
 const TOKEN = 'token'
 const TOKEN_EXPIRES_TIME = 'tokenExpiresTime'
 const TOKEN_TIME_TO_LIVE_IN_MINUTES = 10
+const PAGE = 'page'
