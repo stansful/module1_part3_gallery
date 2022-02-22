1aafb713f8647d31184a1dc05f92300039bb5ea8
stansful <stansful@gmail.com>
2022-02-22 11:11:02 +0300

    feat: add get request to httpService
    httpGet return response from the server, it accepts path and token
    
    MODULE-1-PART-3-gallery

--- js/httpService.js
+++ js/httpService.js
@@ -9,3 +9,14 @@ const httpPost = async (url = '', data = {}) => {
 
     return response.json()
 }
+
+const httpGet = async (path = '', token = '') => {
+    const response = await fetch(path, {
+        method: 'GET',
+        headers: {
+            Authorization: token
+        }
+    })
+
+    return response.json()
+}
